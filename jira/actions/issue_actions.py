d69417d17f9a646c60c844c9daa0f446c6e6a507
Shaked Askayo <shaked@kubiya.io>
2023-07-04 21:57:17 +0300

    added docstrings

--- jira/actions/issue_actions.py
+++ jira/actions/issue_actions.py
@@ -12,10 +12,16 @@ from ..models.common import SimpleResponse
 from .. import action_store as action_store
 
 def get_jira_url(issue_key: str, jira_instance: Jira) -> str:
+    """
+    Returns the URL of an issue
+    """
     return jira_instance.url + '/browse/' + issue_key
 
 @action_store.kubiya_action()
 def create_issue(request: CreateIssueParams) -> CreateIssueResponse:
+    """
+    Creates an issue and returns the issue key and URL
+    """
     jira = get_jira_instance()
     issue = jira.issue_create(
         fields={
@@ -27,16 +33,11 @@ def create_issue(request: CreateIssueParams) -> CreateIssueResponse:
     )
     issue_url = get_jira_url(issue['key'], jira)
     return CreateIssueResponse(issue_url=issue_url, **issue)
-
-@action_store.kubiya_action()
-def run_jql(request: RunJQLParams) -> RunJQLResponse:
-    jira = get_jira_instance()
-    issues = jira.search_issues(request.jql_query)
-    return RunJQLResponse(issues=issues)
-
-
 @action_store.kubiya_action()
 def create_issue(request: CreateIssueParams) -> CreateIssueResponse:
+    """
+    Creates an issue and returns the issue key and URL
+    """
     jira = get_jira_instance()
     issue = jira.issue_create(
         fields={
@@ -50,42 +51,63 @@ def create_issue(request: CreateIssueParams) -> CreateIssueResponse:
 
 @action_store.kubiya_action()
 def transition_issue(request: TransitionIssueParams) -> TransitionIssueResponse:
+    """
+    Transitions an issue and returns the issue key and URL
+    """
     jira = get_jira_instance()
     success = jira.transition_issue(issue_key=request.issue_key, transition=request.transition_name)
     return TransitionIssueResponse(success=success)
 
 @action_store.kubiya_action()
 def link_issues(request: LinkIssuesParams) -> LinkIssuesResponse:
+    """
+    Links two issues and returns the issue key and URL
+    """
     jira = get_jira_instance()
     success = jira.link_issues(issue1=request.issue_key_1, issue2=request.issue_key_2, type=request.link_type_name)
     return LinkIssuesResponse(success=success)
 
 @action_store.kubiya_action()
 def comment_issue(request: CommentIssueParams) -> CommentIssueResponse:
+    """
+    Comments on an issue and returns the comment ID
+    """
     jira = get_jira_instance()
     comment = jira.add_comment(request.issue_key, request.body)
     return CommentIssueResponse(**comment)
 
 @action_store.kubiya_action()
 def update_issue(request: UpdateIssueParams) -> UpdateIssueResponse:
+    """
+    Updates an issue and returns the issue key and URL
+    """
     jira = get_jira_instance()
     success = jira.update_issue(request.issue_key, request.update_dict)
     return UpdateIssueResponse(success=success)
 
 @action_store.kubiya_action()
 def assign_issue(request: AssignIssueParams) -> SimpleResponse:
+    """
+    Assigns an issue to a user and returns the issue key and URL
+    """
     jira = get_jira_instance()
     issue = jira.assign_issue(request.issue_key, request.assignee_name)
     return SimpleResponse(message=f"Issue {request.issue_key} assigned to {request.assignee_name} successfully.")
 
 @action_store.kubiya_action()
 def delete_issue(request: DeleteIssueParams) -> DeleteIssueResponse:
+    """
+    Deletes an issue and returns the issue key and URL
+    """
     jira = get_jira_instance()
     success = jira.delete_issue(request.issue_key)
     return DeleteIssueResponse(success=success)
 
 @action_store.kubiya_action()
 def get_all_issues(request: GetAllIssuesParams) -> GetAllIssuesResponse:
+    """
+    Gets all issues in a project and returns the issue key and URL
+    """
     jira = get_jira_instance()
     issues = jira.search_issues('project={}'.format(request.project_key))
     return GetAllIssuesResponse(issues=issues)
