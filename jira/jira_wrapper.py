4dc19ecf22fa37741398dde4d4bc2d82f6d8bf17
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2023-07-05 17:42:31 +0300

    fix

--- jira/jira_wrapper.py
+++ jira/jira_wrapper.py
@@ -4,3 +4,4 @@ from .secrets import get_jira_secrets
 def get_jira_instance() -> Jira:
     url, username, password = get_jira_secrets()
     return Jira(url=url, username=username, password=password, cloud=True)
+    
