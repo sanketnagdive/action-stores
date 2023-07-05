15f4ea8260a685c97724ecf90eda0d35de791ad9
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2023-07-05 16:50:39 +0300

    test

--- jira/jira_wrapper.py
+++ jira/jira_wrapper.py
@@ -1,7 +1,6 @@
 from atlassian import Jira
 from .secrets import get_jira_secrets
 
-
 def get_jira_instance() -> Jira:
     url, username, password = get_jira_secrets()
     return Jira(url=url, username=username, password=password, cloud=True)
