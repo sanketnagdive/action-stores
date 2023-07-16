201f6a13d20ade67cea78235b119a10c48302465
Mevrat Avraham <mevrat.avraham@kubiya.ai>
2023-07-16 11:05:39 +0300

    [feature] add python health check

---
+++ python/actions/health_check.py
@@ -0,0 +1,15 @@
+from .interpreter import execute_python_code
+import logging as log
+
+def health_check():
+    output = True
+    excepted='run python code'
+    code = 'print("run python code")'
+    
+    result = execute_python_code(code, output)
+    if 'error' in result:
+        log.error("Error: - %s", result['error'])
+        log.error("Traceback: - %s", result['traceback'])
+        return False
+    else:
+        return excepted in result['output']
