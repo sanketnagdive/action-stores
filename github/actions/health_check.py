702050d53e76db9b6dd2c827bcbee2bc544bf388
Mevrat Avraham <mevrat.avraham@kubiya.ai>
2023-07-16 11:39:41 +0300

    [feature] add github health check

--- github/actions/health_check.py
+++ github/actions/health_check.py
@@ -8,31 +8,28 @@ def health_check():
         "org": action_store.secrets.get("GITHUB_ORGANIZATION")
     }
     
-    valid_org = org_check(p["org"])
-    valid_token = token_check(p["token"])
+    valid_org = param_check(p["org"])
+    valid_token = param_check(p["token"])
     valid_user_login = user_login_check(p["token"])
     valid_org_login = org_login_check(p["token"], p["org"])
     
     return valid_org | valid_token | valid_user_login | valid_org_login
 
-def org_check(org: str):
-    return org is None or org == ""
-
-def token_check(token: str):
-    return token is None or token == ""
+def param_check(param: str):
+    return param is None or param == ""
 
 def user_login_check(token: str):
     try:
-        g = Github(token)
-        return g.get_user().login != ""
+        c = Github(token)
+        return c.get_user().login != ""
     except Exception as e:
         log.error("[error]", exception=str(e))
         return False
     
 def org_login_check(token: str, org: str):
     try:
-        g = Github(token)
-        return g.get_organization(org).login != ""
+        c = Github(token)
+        return c.get_organization(org).login != ""
     except Exception as e:
         log.error("[error]", exception=str(e))
         return False
