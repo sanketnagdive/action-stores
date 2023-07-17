c6238dcb20f04ce05abbfc72347a6769d5c186ad
Mevrat Avraham <mevrat.avraham@kubiya.ai>
2023-07-17 16:55:56 +0300

    add pydantic params and response to health check

--- github/actions/health_check.py
+++ github/actions/health_check.py
@@ -1,25 +1,46 @@
 import logging as log
+from typing import List
 from .. import action_store
+from pydantic import BaseModel
 from ..github_wrapper import get_github_instance, get_entity
 from ..secrets import get_github_token, get_github_organization
 
+class HealthRequest(BaseModel):
+    pass
+
+class HealthResponse(BaseModel):
+    params: bool
+    connection: bool
+    errors: List[str]
 
 @action_store.kubiya_action()
-def health_check():
-    pwd = get_github_token()
+def health_check(params: HealthRequest) -> HealthResponse:
+    errors = []
+    valid_params = True
+    token = get_github_token()
     org = get_github_organization()
-    return _param(org) and _param(pwd) and _conn(pwd=pwd, org=org)
+    
+    if _param(token) or _param(org):
+        if _param(org):
+            errors.append("GITHUB_ORGANIZATION is not set")
+        if _param(token):
+            errors.append("GITHUB_TOKEN is not set")
+        valid_params = False
+        
+    valid_connection = _conn(pwd=token, org=org, errs=errors)
+    return HealthResponse(params=valid_params, connection=valid_connection, errors=errors)
 
 
 def _param(p: str) -> bool:
     return p is not None and p != ""
 
 
-def _conn(pwd: str, org: str) -> bool:
+def _conn(pwd: str, org: str, errs: List[str]) -> bool:        
     try:
         c = get_entity(get_github_instance())
         return c.get_user().login != "" and c.get_organization(org).login != ""
     except Exception as e:
         log.error("[error]", exception=str(e))
+        errs.append(f"faild to connect to github: {str(e)}")
         return False
 
