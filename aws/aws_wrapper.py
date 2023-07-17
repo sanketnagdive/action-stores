a3db3f0bca71a0a7a077d47edd919c399e8ee81e
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2023-07-17 11:01:08 +0300

    region should be fetched from env

--- aws/aws_wrapper.py
+++ aws/aws_wrapper.py
@@ -1,23 +1,25 @@
+import os
 import boto3
 from .main_store import store
 
 
 def get_resource(service_name: str):
     """
-    Returns a boto3 client for a specific service.
+    Returns a boto3 resource for a specific service.
 
     Args:
         service_name (str): The name of the AWS service.
 
     Returns:
-        A boto3 client for the specified service.
+        A boto3 resource for the specified service.
     """
-    return boto3.client(service_name,
-                        aws_access_key_id=store.secrets["AWS_ACCESS_KEY_ID"],
-                        aws_secret_access_key=store.secrets["AWS_SECRET_ACCESS_KEY"],
-                        aws_session_token=store.secrets["AWS_SESSION_TOKEN"],
-                        region_name="eu-west-1"
-                        )
+    region = os.getenv("AWS_REGION", "eu-west-1")
+    return boto3.resource(service_name,
+                          aws_access_key_id=store.secrets["AWS_ACCESS_KEY_ID"],
+                          aws_secret_access_key=store.secrets["AWS_SECRET_ACCESS_KEY"],
+                          aws_session_token=store.secrets["AWS_SESSION_TOKEN"],
+                          region_name=region
+                          )
 
 
 def get_client(service_name: str):
@@ -30,9 +32,10 @@ def get_client(service_name: str):
     Returns:
         A boto3 client for the specified service.
     """
+    region = os.getenv("AWS_REGION", "eu-west-1")
     return boto3.client(service_name,
                         aws_access_key_id=store.secrets["AWS_ACCESS_KEY_ID"],
                         aws_secret_access_key=store.secrets["AWS_SECRET_ACCESS_KEY"],
                         aws_session_token=store.secrets["AWS_SESSION_TOKEN"],
-                        region_name="eu-west-1"
+                        region_name=region
                         )
