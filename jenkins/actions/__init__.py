be40d5c238dec918d760125689c7766f0d090bab
michagonzo77 <michagonzo77@gmail.com>
2023-08-02 11:00:39 -0400

    added support for jenkins job with multiple parameters

--- jenkins/actions/__init__.py
+++ jenkins/actions/__init__.py
@@ -4,5 +4,5 @@ from pprint import pprint
 from kubiya import ActionStore, get_secret
 
 
-action_store = ActionStore("jenkins", "0.1.0")
+action_store = ActionStore("jenkins-test", "0.1.0")
 action_store.uses_secrets(["JENKINS_URL", "JENKINS_PASSWORD", "JENKINS_USER"])
