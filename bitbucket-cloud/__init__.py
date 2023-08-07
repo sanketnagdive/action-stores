dcccfe63ae40dcbdcd1e6c7968253f50f5755682
Ram Matzkovsky <ram.matzkovsky@gmail.com>
2023-08-07 16:38:31 +0300

    Refactoring bitbucket-cloud store

--- bitbucket-cloud/__init__.py
+++ bitbucket-cloud/__init__.py
@@ -2,7 +2,7 @@
 import kubiya
 
 action_store = kubiya.ActionStore(
-    "bitbucket-cloud-ram", "0.1.0", "https://seeklogo.com/images/B/bitbucket-logo-D072214725-seeklogo.com.png"
+    "bitbucket-cloud", "0.1.0", "https://seeklogo.com/images/B/bitbucket-logo-D072214725-seeklogo.com.png"
 )
 
 action_store.uses_secrets(
